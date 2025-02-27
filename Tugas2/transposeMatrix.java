7cab0203ff77765c04bb2782b2be9e68eee31573
Gosfist <akunsekolah1234567@gmail.com>
2025-02-27 20:31:55 +0700

    tugas2

--- Tugas2/transposeMatrix.java
+++ Tugas2/transposeMatrix.java
@@ -1,3 +1,25 @@
 public class transposeMatrix {
-    
-}
+    public static void main(String[] args) { // main method
+        // Program operasi matriks
+        int[][] matriksA = { // deklarasi dan inisialisasi
+            {1, 2, 3}, 
+            {4, 5, 6}
+        }; //menutup code
+
+        System.out.println("Sebelum Transpose: "); //mencetak tulisan sebelum transpose
+        for (int i = 0; i < matriksA.length; i++) { //looping ini bagian row
+            for (int j = 0; j < matriksA[0].length; j++) { //looping ini bagian kolom
+                System.out.print(matriksA[i][j]+" "); //mencetak hasil looping
+            } //menutup kode
+            System.out.println(); //mencetak lane kososng
+        } //menutup code
+        System.out.println(); //mencetak lane kososng
+        System.out.println("Sesudah Transpose"); //mencetak tulisan sesudah transpose
+        for (int i = 0; i < matriksA[0].length; i++) { //looping ini bagian kolom
+            for (int j = 0; j < matriksA.length; j++) { //looping ini bagian row
+                System.out.print(matriksA[j][i]+" "); //mencetak hasil looping
+            } //menutup code
+            System.out.println(); //mencetak lane kosong
+        } //menutup code
+    } //menutup code
+} //menutup code
